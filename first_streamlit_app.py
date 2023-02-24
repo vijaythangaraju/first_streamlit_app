1d63995888901b6794cf7a5d64c129b84a3d64d7
vijaythangaraju <125952429+vijaythangaraju@users.noreply.github.com>
2023-02-24 10:23:55 +0530

    Update first_streamlit_app.py
    imported request and added text message for response

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+import requests
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -24,6 +25,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
    
