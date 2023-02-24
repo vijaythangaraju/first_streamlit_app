f52bc3e751c3d7a131b2b68764d5abd0649b334a
vijaythangaraju <125952429+vijaythangaraju@users.noreply.github.com>
2023-02-24 10:25:42 +0530

    Update first_streamlit_app.py
    added header -Fruityvice Fruit Advice!

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -28,5 +28,4 @@ streamlit.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
-
-   
+streamlit.header("Fruityvice Fruit Advice!")
