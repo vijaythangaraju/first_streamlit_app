f154e43229dbe70072c16866b143f8541afa4e15
vijaythangaraju <125952429+vijaythangaraju@users.noreply.github.com>
2023-02-24 10:36:28 +0530

    Update first_streamlit_app.py
    added input in script

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -27,6 +27,9 @@ streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 
 streamlit.header("Fruityvice Fruit Advice!")
 
